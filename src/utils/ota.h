fa9796587dfc0967e2486a1cd350f703d7b22bfd
gideon maina <gmainapro@yahoo.com>
2025-09-03 10:00:51 +0300

    LittelFS OTA update

--- src/utils/ota.h
+++ src/utils/ota.h
@@ -27,35 +27,6 @@ bool otaUpdateFromLittleFS(const char *firmwarePath, const char *backupPath)
     if (!Update.begin(firmwareSize))
     {
         firmware.close();
-        // StreamString error;
-        // Update.printError(error);
-        // Try to restore backup
-        // File backup = LittleFS.open(backupPath, "r");
-        // if (!backup)
-        //     return false;
-        // size_t backupSize = backup.size();
-        // if (!Update.begin(backupSize))
-        // {
-        //     backup.close();
-        //     return false;
-        // }
-        // uint8_t buf[1024];
-        // while (backup.available())
-        // {
-        //     size_t len = backup.read(buf, sizeof(buf));
-        //     if (Update.write(buf, len) != len)
-        //     {
-        //         backup.close();
-        //         return false;
-        //     }
-        // }
-        // if (!Update.end(true))
-        // {
-        //     backup.close();
-        //     return false;
-        // }
-        // backup.close();
-        // ESP.restart();
         Serial.println("Failed to begin update");
         return false; // Indicate update failed, but rollback attempted
     }
@@ -67,8 +38,8 @@ bool otaUpdateFromLittleFS(const char *firmwarePath, const char *backupPath)
         if (Update.write(buf, len) != len)
         {
             firmware.close();
-            // StreamString error;
-            // Update.printError(error);
+            StreamString error;
+            Update.printError(error);
             Serial.println("Error performing update write");
             return false; // Indicate update failed, rollback will be handled after update attempt
         }
@@ -76,8 +47,8 @@ bool otaUpdateFromLittleFS(const char *firmwarePath, const char *backupPath)
     if (!Update.end(true))
     {
         firmware.close();
-        // StreamString error;
-        // Update.printError(error);
+        StreamString error;
+        Update.printError(error);
 
         // Try to restore backup
         if (backupExists)
@@ -131,73 +102,3 @@ bool otaUpdateFromLittleFS(const char *firmwarePath, const char *backupPath)
     ESP.restart();
     return true;
 }
-
-// //! for reference
-// static bool SPIFFSAutoUpdate(String newFirmware, String newMD5)
-// {
-
-//     if (!SPIFFS.exists(newFirmware))
-//     {
-//         Serial.print("No Firmware file found, looking for: ");
-//         Serial.println(newFirmware);
-//         return false;
-//     }
-//     File updateFile = SPIFFS.open(newFirmware, "r");
-//     if (!updateFile)
-//     {
-//         Serial.print("Failed to open : ");
-//         Serial.print(newFirmware);
-//         return false;
-//     }
-
-//     unsigned int free_space = ESP.getFreeSketchSpace();
-//     Serial.print("EsP free sketch space: ");
-//     Serial.println(free_space);
-
-//     if (updateFile.size() >= ESP.getFreeSketchSpace())
-//     {
-//         Serial.println("Cannot update, Firmware too large");
-//         return false;
-//     }
-//     if (!Update.begin(updateFile.size(), U_FLASH))
-//     {
-//         StreamString error;
-//         Update.printError(error);
-
-//         Serial.print("Update.begin returned: "),
-//             Serial.println(error);
-//         return false;
-//     }
-
-//     // set MD5
-//     Update.setMD5(newMD5.c_str());
-
-//     if (Update.writeStream(updateFile) != updateFile.size())
-//     {
-//         StreamString error;
-//         Update.printError(error);
-
-//         Serial.print("Update.writeStream returned: ");
-//         Serial.print(error);
-//         return false;
-//     }
-//     updateFile.close();
-
-//     if (!Update.end())
-//     {
-//         StreamString error;
-//         Update.printError(error);
-
-//         Serial.println("Update.end() returned: ");
-//         Serial.print(error);
-//         return false;
-//     }
-
-//     Serial.println("Erasing SDK config.");
-//     ESP.eraseConfig();
-
-//     Serial.println("Finished successfully.. Rebooting!");
-//     delay(500);
-//     ESP.restart();
-//     return true;
-// }
