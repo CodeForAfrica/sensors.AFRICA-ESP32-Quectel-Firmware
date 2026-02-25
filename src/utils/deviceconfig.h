3bc576edd3f83aa40304f52d7fb6107daf0d7a49
Giddie <gmainapro@yahoo.com>
2026-02-25 10:05:53 +0300

    Improve json config validation

--- src/utils/deviceconfig.h
+++ src/utils/deviceconfig.h
@@ -152,32 +152,33 @@ static void saveConfig(JsonDocument &doc)
 static void loadSavedDeviceConfigs()
 {
 
-    JsonDocument config = getDeviceConfig();
+    JsonDocument config = getDeviceConfig(); // {"ssid":"myssid ","wifiPwd":"mypwd","apn":"myapn","apn_pwd":"myapnpwd","powerSaver":"off"}
 
-    if (config["ssid"] != nullptr)
+    auto hasString = [&](JsonVariant v)
     {
-        if (config["ssid"] != "")
-        {
-            strcpy(DeviceConfig.wifi_sta_ssid, config["ssid"]);
+        return !v.isNull() && v.is<const char *>() && v.as<const char *>()[0] != '\0';
+    };
+
+    if (hasString(config["ssid"]))
+    {
+        strcpy(DeviceConfig.wifi_sta_ssid, config["ssid"]);
+        if (hasString(config["wifiPwd"]))
             strcpy(DeviceConfig.wifi_sta_pwd, config["wifiPwd"]);
-        }
     }
-    if (config["apn"] != nullptr)
+
+    if (hasString(config["apn"]))
     {
-        if (config["apn"] != "")
-        {
-            strcpy(DeviceConfig.wifi_sta_ssid, config["apn"]);
-            strcpy(DeviceConfig.wifi_sta_pwd, config["apnPwd"]);
-        }
+        strcpy(DeviceConfig.gsm_apn, config["apn"]);
+        if (hasString(config["apn_pwd"]))
+            strcpy(DeviceConfig.gsm_apn_pwd, config["apn_pwd"]);
     }
-    if (config["simPin"] != nullptr)
+
+    if (hasString(config["simPin"]))
     {
-        if (config["simPin"] != "")
-        {
-            strcpy(DeviceConfig.sim_pin, config["simPin"]);
-        }
+        strcpy(DeviceConfig.sim_pin, config["simPin"]);
     }
-    if (config["powerSaver"] != nullptr)
+
+    if (hasString(config["powerSaver"]))
     {
         if (config["powerSaver"] == "on")
             DeviceConfig.power_saving_mode = true;
@@ -187,3 +188,64 @@ static void loadSavedDeviceConfigs()
 }
 
 #endif
+
+// static void loadSavedDeviceConfigs()
+// {
+
+//     JsonDocument config = getDeviceConfig();
+
+//     // helper lambda that returns true if a variant contains a non‑empty string
+//     auto hasString = [&](JsonVariant v)
+//     {
+//         return !v.isNull() && v.is<const char *>() && v.as<const char *>()[0] != '\0';
+//     };
+
+//     // ---------- wifi credentials ----------
+//     if (hasString(config["ssid"]))
+//     {
+//         Serial.println("SSID found");
+//         Serial.println("configuring wifi");
+//         strcpy(DeviceConfig.wifi_sta_ssid, config["ssid"].as<const char *>());
+//         if (hasString(config["wifiPwd"]))
+//             strcpy(DeviceConfig.wifi_sta_pwd, config["wifiPwd"].as<const char *>());
+//     }
+
+//     // ---------- gsm/apn ---------
+//     if (hasString(config["apn"]))
+//     {
+//         Serial.println("apn found");
+//         Serial.println("configuring gsm");
+//         strcpy(DeviceConfig.gsm_apn, config["apn"].as<const char *>());
+//         if (hasString(config["apn_pwd"]))
+//             strcpy(DeviceConfig.gsm_apn_pwd, config["apn_pwd"].as<const char *>());
+//     }
+
+//     // ---------- sim pin ----------
+//     if (hasString(config["simPin"]))
+//     {
+//         Serial.println("SIM pin found");
+//         Serial.println("configuring sim");
+//         strcpy(DeviceConfig.sim_pin, config["simPin"].as<const char *>());
+//     }
+//     else
+//     {
+//         Serial.println("SIM pin not found or invalid");
+//     }
+
+//     // ---------- power saver ----------
+//     if (!config["powerSaver"].isNull())
+//     {
+//         Serial.println("Power saver found");
+//         if (config["powerSaver"] == "on")
+//         {
+//             Serial.println("configuring powersaver");
+//             DeviceConfig.power_saving_mode = true;
+//         }
+//         else
+//             DeviceConfig.power_saving_mode = false;
+//     }
+//     else
+//     {
+//         Serial.println("Power saver not found");
+//     }
+// }
