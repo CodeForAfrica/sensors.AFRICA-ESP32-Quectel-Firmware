1380c9df32f57a0ef2a802f4832f744e39ae40af
Giddie <gmainapro@yahoo.com>
2026-06-09 13:32:23 +0300

    rename get config path

--- src/webserver/asyncserver.cpp
+++ src/webserver/asyncserver.cpp
@@ -55,7 +55,7 @@ void setup_webserver()
             { request->send(LittleFS, "/icons/cell_tower.svg", "image/svg+xml"); });
   server.on("/device-id", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(200, "text/plain", AP_SSID); });
-  server.on("/device-info.json", [](AsyncWebServerRequest *request)
+  server.on("/device-config.json", [](AsyncWebServerRequest *request)
             {
         JsonDocument data=getDeviceConfig();
         String data_str;
